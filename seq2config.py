02918ea49320b013e6d82b2c3042ae1a49a80090
Dray988 <76453607+Dray988@users.noreply.github.com>
2025-06-03 16:04:08 -0400

    Your descriptive commit message here

--- seq2config.py
+++ seq2config.py
@@ -1,63 +1,71 @@
 import numpy as np
 from sys import argv
+import os
 
-'''
-For details on using this script, please consult ReadMe.txt
-'''
+# Minimal addition starts here
+user_input = input("Enter sequence format (1 = one-letter, 2 = three-letter): ").strip()
+if user_input == "1":
+    try:
+        with open("seq.txt", "r", encoding="utf-8") as infile:
+            content = infile.read()
+    except UnicodeDecodeError:
+        print("Could not read seq.txt (non-text or corrupted file)")
+        exit()
+
+    content = ''.join(content.split())
+    if content:
+        with open("seq.txt", "w", encoding="utf-8") as outfile:
+            outfile.write('\n'.join(content))
+        print("Preprocessed seq.txt for 1-letter format.")
+    else:
+        print("seq.txt is empty after cleaning.")
+        exit()
+
+''' For details on using this script, please consult ReadMe.txt '''
 
 if len(argv) > 1:
+    script = argv[0]
+    seqType = int(argv[1])
 
-	script = argv[0]
-	seqType = int(argv[1])
+    if seqType == 3:
+        seq = str(argv[2])
+        nres = int(len(seq)/3)
+        charges = np.loadtxt('aa_charges_three_letters.txt', dtype=str)
+        types = np.loadtxt('aa_types_three_letters.txt', dtype=str)
 
-	if seqType == 3:
-		seq = str(argv[2])
-		nres = int(len(seq)/3)
-		charges = np.loadtxt('aa_charges_three_letters.txt', dtype=str)
-		types = np.loadtxt('aa_types_three_letters.txt', dtype=str)
+        typescharges = []
 
-		typescharges = []
+        count = 0
+        for i in range(nres):
+            k, = np.where(charges[:,0] == seq[count:count+3])
+            p, = np.where(types[:,0] == seq[count:count+3])
+            count += 3
+            typescharges.append([int(types[p,1]),float(charges[k,1])])
 
-		count = 0
-		for i in range(nres):
-			k, = np.where(charges[:,0] == seq[count:count+3])
-			p, = np.where(types[:,0] == seq[count:count+3])
-			count += 3
-			typescharges.append([int(types[p,1]),float(charges[k,1])])
+    elif seqType == 1:
+        seq = str(argv[2])
+        nres = len(seq)
+        charges = np.loadtxt('aa_charges_one_letter.txt', dtype=str)
+        types = np.loadtxt('aa_types_one_letter.txt', dtype=str)
 
-	elif seqType == 1:
-		seq = str(argv[2])
-		nres = len(seq)
-		charges = np.loadtxt('aa_charges_one_letter.txt', dtype=str)
-		types = np.loadtxt('aa_types_one_letter.txt', dtype=str)
+        typescharges = []
 
-		typescharges = []
-
-		for i in range(nres):
-			k, = np.where(charges[:,0] == seq[i])
-			p, = np.where(types[:,0] == seq[i])
-			typescharges.append([int(types[p,1]),float(charges[k,1])])
-
+        for i in range(nres):
+            k, = np.where(charges[:,0] == seq[i])
+            p, = np.where(types[:,0] == seq[i])
+            typescharges.append([int(types[p,1]),float(charges[k,1])])
 else:
-    user_input = input("Enter sequence format (1 = one-letter, 2 = three-letter): ").strip()
 
     seq = np.loadtxt('seq.txt', dtype=str)
-    
     # Optionally preprocess for one-letter format
-    if user_input == "1":
-        if seq.ndim > 1:
-            seq = ''.join(seq.flatten())
-        else:
-            seq = ''.join(seq)
-        seqType = 1
-    else:
-        seqType = len(seq[0])
+
+    seqType = len(seq[0])
 
     if seqType == 3:
         charges = np.loadtxt('aa_charges_three_letters.txt', dtype=str)
         types = np.loadtxt('aa_types_three_letters.txt', dtype=str)
         nres = len(seq)
-        
+
         typescharges = []
 
         for i in range(nres):
@@ -69,7 +77,7 @@ else:
         charges = np.loadtxt('aa_charges_one_letter.txt', dtype=str)
         types = np.loadtxt('aa_types_one_letter.txt', dtype=str)
         nres = len(seq)
-        
+
         typescharges = []
 
         for i in range(nres):
@@ -77,13 +85,8 @@ else:
             p, = np.where(types[:,0] == seq[i])
             typescharges.append([int(types[p,1]),float(charges[k,1])])
 
-
-
-
 total_atoms = nres
-
 no_atom_types = 20
-
 out = open('myconfig.dat','w')
 out.write('LAMMPS data file for IDP\n\n')
 out.write('%d atoms\n'%total_atoms)
@@ -100,27 +103,26 @@ out.write('%5f   %5f  zlo zhi\n\n'%(min_box,max_box))
 
 out.write('Masses\n\n')
 
-mass_dict = {1:   131.199997,    
-             2:   57.0499992,    
-             3:   128.199997,    
-             4:   101.099998,    
-             5:   156.199997,    
-             6:   71.0800018,    
-             7:   115.099998,    
-             8:   129.100006,    
-             9:   163.199997,    
-             10:   99.0699997,    
-             11:   113.199997,    
-             12:   128.100006,    
-             13:   186.199997,    
-             14:   147.199997,    
-             15:   87.0800018,    
-             16:   137.100006,    
-             17:   114.099998,    
-             18:   97.1200027,    
-             19:   103.099998,    
-             20:   113.199997
-}
+mass_dict = {1:   131.199997,
+             2:   57.0499992,
+             3:   128.199997,
+             4:   101.099998,
+             5:   156.199997,
+             6:   71.0800018,
+             7:   115.099998,
+             8:   129.100006,
+             9:   163.199997,
+             10:   99.0699997,
+             11:   113.199997,
+             12:   128.100006,
+             13:   186.199997,
+             14:   147.199997,
+             15:   87.0800018,
+             16:   137.100006,
+             17:   114.099998,
+             18:   97.1200027,
+             19:   103.099998,
+             20:   113.199997}
 
 count = 1
 while count <= no_atom_types:
@@ -128,7 +130,6 @@ while count <= no_atom_types:
     count += 1
 
 out.write('\nAtoms\n\n')
-
 
 xcoord=0.0
 for k,v in enumerate(typescharges):
